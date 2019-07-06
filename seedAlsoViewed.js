caa38bcc96d9761ad236430bc61e356f96deacc2
amol gajewar <amolgajewar@gmail.com>
2019-07-06 16:57:37 -0700

    removed condition for discountToken to be greater than 5

--- seedAlsoViewed.js
+++ seedAlsoViewed.js
@@ -1,24 +1,23 @@
-var mysql = require("mysql");
-var Faker = require ("Faker");
+var mysql = require('mysql');
+var Faker = require ('Faker');
 
 
-var dbConnection = mysql.createConnection({
+var dbConnection = mysql.createConnection ({
   user: 'root', // FILL IN WITH YOUR USERNAME
   password: '', // FILL IN WITH YOUR PASSWORD
   database: 'alsoviewed' //DON'T CHANGE
 });
 
 dbConnection.connect(function(err) {
-  if(err) {
+  if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     console.log('database connected: success');
   }
 });
 
 dbConnection.query('truncate alsovieweditems', function(err) {
-  if(err) {
+  if (err) {
     console.log(err);
   } else {
     console.log('truncated table \'alsovieweditems\' before seeding' + err);
@@ -29,7 +28,7 @@ dbConnection.query('truncate alsovieweditems', function(err) {
 /// generates random product company / brand name
 var getItemTitle = function () {
   return Faker.Company.companyName();
-}
+};
 
 /// generates random old price between 10 to 10,000
 /// else returns null as old price
@@ -45,7 +44,7 @@ var getOldPrice = function () {
     return null;
   }
 
-}
+};
 
 /// if oldprice is not null generates current price based of old price
 /// else generates random current price between 10 to 10,000
@@ -53,36 +52,31 @@ var getCurrentPrice = function (oldPrice) {
   // generates discount token
   var discountToken = Math.round( Math.random() * 10 );
 
-  if (discountToken < 5) {
-    // discount has to be less than 50 % hence adding 4
-    discountToken = discountToken + 4;
-  }
-
   if (oldPrice) {
     return parseFloat((oldPrice - (oldPrice * (discountToken / 100))).toFixed(2));
   } else {
     return parseFloat((Math.random() * (10000 - 10)).toFixed(2));
   }
-}
+};
 
 /// uses https://picsum.photos/
 var getImageUrl = function () {
   return 'https://picsum.photos/200/300';
-}
+};
 
 /// randomly returns free shipping true or false
 var getFreeShipping = function () {
   return (Math.round(Math.random() * 10) > 5);
-}
+};
 
 /// based on free shipping true or false returns random shipping cost under 100
 var getShippingCost = function (freeSheeping) {
-  if(!freeSheeping) {
+  if (!freeSheeping) {
     return parseFloat((Math.random() * (100 - 10)).toFixed(2));
   } else {
     return null;
   }
-}
+};
 
 ///generate seed data with id 1 to given
 var seedAlsoViewedItems = [];
@@ -105,7 +99,9 @@ var queryString = 'insert into alsovieweditems (id, image, title, oldprice, curr
 var queryArgs = seedAlsoViewedItems;
 
 dbConnection.query(queryString, [queryArgs], function(err) {
-  if(err) throw err;
+  if (err) {
+    throw err;
+  }
 
   console.log('seeding data completed closing database connection');
   dbConnection.end();
