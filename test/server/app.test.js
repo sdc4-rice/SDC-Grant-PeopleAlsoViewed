863b78256675f88813c7d772fba563ed4e97b151
amol gajewar <amolgajewar@gmail.com>
2019-07-11 12:05:43 -0700

    made changes as per style guide

--- test/server/app.test.js
+++ test/server/app.test.js
@@ -1,10 +1,8 @@
 const request = require('supertest');
-const express = require('express');
 const app = require('../../server/app.js');
 
 
-test('GET request on /api/alsovieweditems should return 100 items', done => {
-
+test('GET request on /api/alsovieweditems should return 100 items', (done) => {
   request(app)
     .get('/api/alsovieweditems')
     .expect('Content-Type', /json/)
@@ -13,17 +11,15 @@ test('GET request on /api/alsovieweditems should return 100 items', done => {
       if (err) {
         done(err);
       }
-      var expectedLength = 100;
-      var actualLength = res.body.length;
+      const expectedLength = 100;
+      const actualLength = res.body.length;
       expect(actualLength).toBe(expectedLength);
       done();
     });
-
 });
 
 
-test('GET request on /api/alsovieweditems should return items with properties id, image, title, oldprice, currentprice, freeshipping, shippingcost', done => {
-
+test('GET request on /api/alsovieweditems should return items with properties id, image, title, oldprice, currentprice, freeshipping, shippingcost', (done) => {
   request(app)
     .get('/api/alsovieweditems')
     .expect('Content-Type', /json/)
@@ -32,16 +28,14 @@ test('GET request on /api/alsovieweditems should return items with properties id
       if (err) {
         done(err);
       }
-      var expectedKeys = ['id', 'image', 'title', 'oldprice', 'currentprice', 'freeshipping', 'shippingcost'];
-      var actualKeys = Object.keys(res.body[0]);
+      const expectedKeys = ['id', 'image', 'title', 'oldprice', 'currentprice', 'freeshipping', 'shippingcost'];
+      const actualKeys = Object.keys(res.body[0]);
       expect(actualKeys).toEqual(expect.arrayContaining(expectedKeys));
       done();
     });
-
 });
 
-test('GET request should return image property set to https://picsum.photos/id/1/200/300 for item with id = 1', done => {
-
+test('GET request should return image property set to https://picsum.photos/id/1/200/300 for item with id = 1', (done) => {
   request(app)
     .get('/api/alsovieweditems')
     .expect('Content-type', /json/)
@@ -50,14 +44,14 @@ test('GET request should return image property set to https://picsum.photos/id/1
       if (err) {
         done(err);
       }
-      var expectedImageUrl = 'https://picsum.photos/id/1/200/300';
-      var actualImageUrl = res.body[0]['image'];
+      const expectedImageUrl = 'https://picsum.photos/id/1/200/300';
+      const actualImageUrl = res.body[0].image;
       expect(actualImageUrl).toBe(expectedImageUrl);
       done();
     });
 });
 
-test('GET request for /api/alsovieweditems/categoryids should return all category ids assigned to items', done => {
+test('GET request for /api/alsovieweditems/categoryids should return all category ids assigned to items', (done) => {
   request(app)
     .get('/api/alsovieweditems/categoryids')
     .expect('Content-Type', /json/)
@@ -66,17 +60,16 @@ test('GET request for /api/alsovieweditems/categoryids should return all categor
       if (err) {
         done(err);
       }
-      var expectedCategoryIds = [1, 2, 3, 4, 5, 6, 7, 8];
-      for (var i = 0; i < res.body.length; i++) {
-        var actualCategoryId = res.body[i];
+      const expectedCategoryIds = [1, 2, 3, 4, 5, 6, 7, 8];
+      for (let i = 0; i < res.body.length; i += 1) {
+        const actualCategoryId = res.body[i];
         expect(expectedCategoryIds.includes(actualCategoryId)).toBeTruthy();
       }
       done();
     });
 });
 
-test('GET request on /api/alsovieweditems/categoryid/5 should return list of items with categoryid = 5', done => {
-
+test('GET request on /api/alsovieweditems/categoryid/5 should return list of items with categoryid = 5', (done) => {
   request(app)
     .get('/api/alsovieweditems/categoryid/5')
     .expect('Content-Type', /json/)
@@ -85,18 +78,16 @@ test('GET request on /api/alsovieweditems/categoryid/5 should return list of ite
       if (err) {
         done(err);
       }
-      var expectedCategoryId = 5;
-      for (var i = 0; i < res.body.length; i++) {
-        var actualCategoryId = res.body[i].categoryid;
+      const expectedCategoryId = 5;
+      for (let i = 0; i < res.body.length; i += 1) {
+        const actualCategoryId = res.body[i].categoryid;
         expect(actualCategoryId).toBe(expectedCategoryId);
       }
       done();
     });
-
 });
 
-test('GET request on /api/alsovieweditems/startid/15/endid/24 should return 10 items with ids from 15 to 24', done => {
-
+test('GET request on /api/alsovieweditems/startid/15/endid/24 should return 10 items with ids from 15 to 24', (done) => {
   request(app)
     .get('/api/alsovieweditems/startid/15/endid/24')
     .expect('Content-Type', /json/)
@@ -105,16 +96,16 @@ test('GET request on /api/alsovieweditems/startid/15/endid/24 should return 10 i
       if (err) {
         done(err);
       }
-      var expectedResultLength = 10;
-      var actualResultLength = res.body.length;
+      const expectedResultLength = 10;
+      const actualResultLength = res.body.length;
       expect(actualResultLength).toBe(expectedResultLength);
 
-      var expectedStartId = 15;
-      var actualStartId = res.body[0].id;
+      const expectedStartId = 15;
+      const actualStartId = res.body[0].id;
       expect(actualStartId).toBe(expectedStartId);
 
-      var expectedEndId = 24;
-      var actualEndId = res.body[res.body.length - 1].id;
+      const expectedEndId = 24;
+      const actualEndId = res.body[res.body.length - 1].id;
       expect(actualEndId).toBe(expectedEndId);
 
       done();
