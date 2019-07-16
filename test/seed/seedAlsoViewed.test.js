02f0fe2cf144bbe2f9dbdffa306bf176e0d5a57a
amol gajewar <amolgajewar@gmail.com>
2019-07-16 16:31:42 -0700

    made changes to import variables from .env file

--- test/seed/seedAlsoViewed.test.js
+++ test/seed/seedAlsoViewed.test.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const dbConnection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'alsoviewed',
+  host: process.env.MYSQL_DB_HOST || 'localhost',
+  user: process.env.MYSQL_DB_USERNAME || 'root',
+  password: process.env.MYSQL_DB_PASSWORD || '',
+  database: process.env.MYSQL_DB_DATABASE || 'alsoviewed',
 });
 
 
@@ -25,30 +27,33 @@ test('database alsoviewed should have table named alsovieweditems', (done) => {
   });
 });
 
-test('seeding database should result into 100 entries in alsoviweditems table', (done) => {
+test('seeding database should result into TOTAL_ITEMS spcified in .env file for alsovieweditems table', (done) => {
+  const totalItems = Number(process.env.TOTAL_ITEMS);
   const queryString = 'select * from alsovieweditems';
   const queryArgs = [];
 
   dbConnection.query(queryString, queryArgs, (err, results) => {
-    expect(results.length).toBe(100);
+    expect(results.length).toBe(totalItems);
     done();
   });
 });
 
 
-test('alsovieweditems should have entry with id 1', (done) => {
-  const queryString = 'select * from alsovieweditems where id = 1';
+test('alsovieweditems should have entry with id = START_ITEM_ID specified in .env file', (done) => {
+  const startId = Number(process.env.START_ITEM_ID);
+  const queryString = `select * from alsovieweditems where id = ${startId}`;
   const queryArgs = [];
 
   dbConnection.query(queryString, queryArgs, (err, results) => {
     const json = JSON.parse(JSON.stringify(results[0]));
-    expect(json.id).toBe(1);
+    expect(json.id).toBe(startId);
     done();
   });
 });
 
 test('read query on alsovieweditems should return object with colomn names as properties', (done) => {
-  const queryString = 'select * from alsovieweditems where id = 1';
+  const startId = Number(process.env.START_ITEM_ID);
+  const queryString = `select * from alsovieweditems where id = ${startId}`;
   const queryArgs = [];
 
   dbConnection.query(queryString, queryArgs, (err, results) => {
